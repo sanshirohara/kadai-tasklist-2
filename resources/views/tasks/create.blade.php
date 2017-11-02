ad8f081caa2ec30f34f0eed681a1f11cf82baab6
sanshirohara <sanshirohara01@gmail.com>
2017-11-02 14:18:31 +0000

    shuusei

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
-
+    
+    {{ '<p style="color: red;">htmlentities 関数に通した場合</p>' }}
+    {!! '<p style="color: red;">htmlentities 関数に通さなかった場合</p>' !!}
+    
     <div class="row">
         <div class="col-lg-6 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
